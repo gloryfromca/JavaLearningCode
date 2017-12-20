32eba4deabc5867a064cc4e91bde66a90c3aef36
gloryfromca <huizhang1995@gmail.com>
2017-12-20 23:47:51 +0800

    Practice for a Java beginner:reverseString

--- dataStructurePractice/ReverseString.java
+++ dataStructurePractice/ReverseString.java
@@ -1,11 +1,36 @@
 package dataStructurePractice;
 
-
 public class ReverseString {
+
+	private String out;
+	private String in;
+
+	public ReverseString(String in) {
+		this.in = in;
+	}
+
+	public String doRev() {
+		int len = in.length();
+		MyStack<Character> stack = new MyStack<Character>(len);
+		for (int i = 0; i < len; i++) {
+			stack.push(in.charAt(i));
+		}
+		out = "";
+		while (!stack.isEmpty()) {
+			out = out + stack.pop().get();
+		}
+
+		return out;
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		String s = "zhanghui";
+		ReverseString res = new ReverseString(s);
+		String output = res.doRev();
+		System.out.println(output);
+
 	}
 
 }
--- genericsPractice/Wildcards.java
+++ genericsPractice/Wildcards.java
@@ -2,39 +2,26 @@ package genericsPractice;
 
 import java.util.ArrayList;
 import java.util.List;
-
-class Fruit {
-}
-
-class Apple extends Fruit {
-}
-class GoodApple extends Apple {
-}
-
-class Orange extends Fruit {
-
-}
-
+import genericsPractice.Covariance.*;
 public class Wildcards {
 
 	public static void main(String[] args) {
 
 		// fruitBag is producer, we just get fruit from it which never be added by
 		// any type of fruit(We can just add null to it).
-		//Usually as return representing more strict return value.
+		// Usually as return representing more strict return value.
 		List<? extends Fruit> fruitBag = new ArrayList<Apple>();
 		// fruitBag.add(new Fruit());
 		// fruitBag.add(new Apple());
 		// fruitBag.add(new Orange());
-		
+
 		fruitBag.add(null);
 		List<? super Apple> fruitBag1 = new ArrayList<Fruit>();
 		fruitBag1.add(new GoodApple());
 		fruitBag1.add(new Apple());
-		//fruitBag1.add(new Orange());
-		//code below will raise Error can't pass through compilation
-		//fruitBag1.add(new Object());
-		
+		// fruitBag1.add(new Orange());
+		// code below will raise Error can't pass through compilation
+		// fruitBag1.add(new Object());
 
 	}
 
